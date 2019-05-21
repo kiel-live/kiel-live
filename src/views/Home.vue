385957aa1f02d88244698eb5e4973c2d99f83a3c
Anton Bracke <anton.bracke.com@gmail.com>
2019-05-21 15:08:00 +0200

    added some style, and propper displaying of special html chars

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="home">
-    <TextInput @input="searchStop" :debounce="true" :wait="400" placeholder="Haltestelle" autofocus />
+    <TextInput class="search" @input="searchStop" :debounce="true" :wait="400" placeholder="Haltestelle" autofocus />
     <div v-if="stops" class="stops">
-      <div v-for="stop in stops" :key="stop.id" @click="$router.push({ name: 'stop', params: { stop: stop.id } })" class="stop">{{ stop.name }}</div>
+      <router-link v-for="stop in stops" :key="stop.id" :to="{ name: 'stop', params: { stop: stop.id } }" class="stop">
+        <span class="name">{{ unEscapeHtml(stop.name) }}</span>
+        <i class="icon fas fa-arrow-right"></i>
+      </router-link>
     </div>
   </div>
 </template>
@@ -26,6 +29,21 @@ export default {
     searchStop(query) {
       Api.emit('stop:search', query);
     },
+    unEscapeHtml(unsafe) {
+      return unsafe
+        .replace('&amp;', /&/g)
+        .replace('&lt;', /</g)
+        .replace('&gt;', />/g)
+        .replace('&quot;', /"/g)
+        .replace('&#039;', /'/g)
+        .replace('&auml;', 'ä')
+        .replace('&Auml;', 'Ä')
+        .replace('&ouml;', 'ö')
+        .replace('&Ouml;', 'Ö')
+        .replace('&uuml;', 'ü')
+        .replace('&Uuml;', 'Ü')
+        .replace('&szlig;', 'ß');
+    },
   },
   mounted() {
     Api.on('stop:search', (stops) => {
@@ -44,3 +62,46 @@ export default {
   },
 };
 </script>
+
+<style lang="scss" scoped>
+  .home {
+    display: flex;
+    flex-flow: column;
+    margin: 0 auto;
+    width: 100%;
+    max-width: 40rem;
+  }
+
+  .search {
+    margin: 0 .5rem;
+    margin-top: 1rem;
+    width: calc(100% - 1rem);
+  }
+
+  .stops {
+    display: flex;
+    flex-flow: column;
+  }
+
+  .stop {
+    display: flex;
+    padding: 1rem;
+    flex-flow: row;
+    width: 100%;
+    box-shadow: inset 0 -1px 0 0 rgba(100,121,143,0.122);
+    text-align: left;
+    text-decoration: none;
+    color: #000;
+    cursor: pointer;
+
+    &:hover {
+      -webkit-box-shadow: inset 1px 0 0 #dadce0, inset -1px 0 0 #dadce0, 0 1px 2px 0 rgba(60,64,67,.3), 0 1px 3px 1px rgba(60,64,67,.15);
+      box-shadow: inset 1px 0 0 #dadce0, inset -1px 0 0 #dadce0, 0 1px 2px 0 rgba(60,64,67,.3), 0 1px 3px 1px rgba(60,64,67,.15);
+      z-index: 1;
+    }
+
+    .icon {
+      margin-left: auto;
+    }
+  }
+</style>
