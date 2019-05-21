376c2e69e72933619e932ea1d3295000ee1bb754
Anton Bracke <anton.bracke.com@gmail.com>
2019-05-21 10:36:26 +0200

    added linter rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,8 +10,18 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-param-reassign': ['error', { 'props': false }],
+    'no-underscore-dangle': 'off',
   },
   parserOptions: {
     parser: 'babel-eslint',
   },
+  overrides: [
+    {
+      files: ['*.vue'],
+      rules: {
+        'max-len': 'off'
+      }
+    }
+  ],
 };
