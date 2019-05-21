ba330649e1e3b34ada1e66620b31a99846a3d277
Anton Bracke <anton.bracke.com@gmail.com>
2019-05-21 10:39:18 +0200

    changed home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,46 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TextInput @input="searchStop" :debounce="true" :wait="400" placeholder="Haltestelle" autofocus />
+    <div v-if="stops" class="stops">
+      <div v-for="stop in stops" :key="stop.id" @click="$router.push({ name: 'stop', params: { stop: stop.id } })" class="stop">{{ stop.name }}</div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import Api from '@/api';
+
+import TextInput from '@/components/TextInput.vue';
 
 export default {
   name: 'home',
   components: {
-    HelloWorld,
+    TextInput,
+  },
+  data() {
+    return {
+      stops: null,
+    };
+  },
+  methods: {
+    searchStop(query) {
+      Api.emit('stop:search', query);
+    },
+  },
+  mounted() {
+    Api.on('stop:search', (stops) => {
+      this.stops = [];
+
+      const regexp = RegExp('<li stop="(.*?)">(.*?)</li>', 'g');
+      let matches = regexp.exec(stops);
+      while (matches) {
+        this.stops.push({
+          id: matches[1],
+          name: matches[2],
+        });
+        matches = regexp.exec(stops);
+      }
+    });
   },
 };
 </script>
