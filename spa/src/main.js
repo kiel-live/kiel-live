2cdb1884eac4fd92cbaf03ccc08045e51f46ee70
Anton Bracke <anton.bracke.com@gmail.com>
2019-06-20 21:35:36 +0200

    added vue-matomo plugin for spa tracking

--- spa/src/main.js
+++ spa/src/main.js
@@ -4,7 +4,7 @@ import router from './router';
 import store from './store';
 import './api';
 import './registerServiceWorker';
-import './tracking';
+import tracking from './tracking';
 
 Vue.config.productionTip = false;
 
@@ -13,3 +13,5 @@ new Vue({
   store,
   render: h => h(App),
 }).$mount('#app');
+
+tracking.init(router);
