f545d66557953fe8350da9b112c214ef8ca5d895
Anton Bracke <anton.bracke.com@gmail.com>
2019-06-20 08:15:45 +0200

    fixed production building

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #! /bin/sh
 
 # check initial setup
-if [ ! -f /app/setup_done ]; then
+if [[ ! -f /app/setup_done ]] || [[ -n "${NODE_ENV}" ]] || [[ "${NODE_ENV}" == "development" ]]; then
   cd /app/spa
-  npm install --silent
+  NODE_ENV= npm install --silent
   npm run build
 
   echo "" > /app/setup_done
@@ -11,7 +11,6 @@ fi
 
 if [[ -n "${NODE_ENV}" ]] && [[ "${NODE_ENV}" == "development" ]]; then
   cd /app && npm run dev
-  cd /app/spa && npm run build # rebuild on each start
 else
   cd /app && npm run start
 fi
