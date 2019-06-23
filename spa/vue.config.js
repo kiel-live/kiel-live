ee6264a0e9e65fcf187c2ade000b9e1a0e7c7560
Lukas Hass <lukas@slucky.de>
2019-06-23 19:06:10 +0200

    pwa config

--- spa/vue.config.js
+++ spa/vue.config.js
@@ -13,5 +13,15 @@ module.exports = {
     workboxOptions: {
       skipWaiting: true,
     },
+    iconPaths: {
+      favicon32: 'img/icons/favicon-32x32.png',
+      favicon16: 'img/icons/favicon-16x16.png',
+      appleTouchIcon: 'img/icons/apple-touch-icon.png',
+      maskIcon: 'img/icons/safari-pinned-tab.svg',
+      msTileImage: 'img/icons/mstile-150x150.png'
+    },
+    themeColor: '#2c3e50',
+    msTileColor: '#ffffff',
+    name: "Kiel Live"
   },
 };
