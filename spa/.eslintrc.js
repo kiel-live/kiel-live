bb09cb82a10de02227dc1900a31be583a8af1062
Anton Bracke <anton.bracke.com@gmail.com>
2019-12-21 22:57:49 +0100

    yarn & updated deps

--- spa/.eslintrc.js
+++ spa/.eslintrc.js
@@ -1,21 +1,26 @@
 module.exports = {
   root: true,
+
   env: {
     node: true,
   },
+
   extends: [
     'plugin:vue/essential',
     '@vue/airbnb',
   ],
+
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-param-reassign': ['error', { 'props': false }],
+    'no-param-reassign': ['error', { props: false }],
     'no-underscore-dangle': 'off',
   },
+
   parserOptions: {
     parser: 'babel-eslint',
   },
+
   overrides: [
     {
       files: ['*.vue'],
--- start.sh
+++ start.sh
@@ -7,15 +7,15 @@ if [[ ! -f /app/setup_done ]] || [[ -n "${NODE_ENV}" ]] || [[ "${NODE_ENV}" == "
   echo "Building spa ..."
   cd /app/spa
   export VUE_APP_VERSION=$(date)
-  NODE_ENV= npm install --silent
-  npm run build
+  NODE_ENV= yarn install --silent
+  yarn run build
 
   echo "" > /app/setup_done
   echo "Setup done!"
 fi
 
 if [[ -n "${NODE_ENV}" ]] && [[ "${NODE_ENV}" == "development" ]]; then
-  cd /app && npm run dev
+  cd /app && yarn run dev
 else
-  cd /app && npm run start
+  cd /app && yarn run start
 fi
