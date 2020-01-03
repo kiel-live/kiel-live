9acae1a74d931040d5fdf9cbf5eda3793ca93903
Anton Bracke <anton.bracke.com@gmail.com>
2020-01-03 19:20:34 +0100

    added eslint

---
+++ backend/.eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-param-reassign': ['error', { props: false }],
+    'no-underscore-dangle': 'off',
+  },
+};
