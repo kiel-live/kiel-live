21f430e49f7246476d93ed2cf96a145051b7bd91
Anton Bracke <anton.bracke.com@gmail.com>
2020-03-03 11:19:32 +0100

    added workbox dependencies

--- spa/src/sw.js
+++ spa/src/sw.js
@@ -1,5 +1,8 @@
 /* eslint-disable no-restricted-globals */
-/* global workbox */
+/* global workbox, importScripts */
+
+// Import workbox-sw, which defines the global `workbox` object.
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js');
 
 // This is the code piece that GenerateSW mode can't provide for us.
 // This code listens for the user's confirmation to update the app.
