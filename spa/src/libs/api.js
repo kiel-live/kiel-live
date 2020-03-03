b7e10adfd97fa84eadca96997f9744bc2d1f1a91
Anton Bracke <anton.bracke.com@gmail.com>
2020-03-03 12:29:14 +0100

    changed to use websockets only

--- spa/src/libs/api.js
+++ spa/src/libs/api.js
@@ -1,5 +1,8 @@
 import io from 'socket.io-client';
 
-const socket = io('', { path: '/api/socket' });
+const socket = io({
+  path: '/api/socket',
+  transports: ['websocket'], // websocket only
+});
 
 export default socket;
