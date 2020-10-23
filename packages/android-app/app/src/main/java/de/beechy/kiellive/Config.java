0885428e0e0aae7a797541ebbb1cded6c65f0f56
Anton Bracke <anton@ju60.de>
2020-10-23 23:33:12 +0200

    change url

--- packages/android-app/app/src/main/java/de/beechy/kiellive/Config.java
+++ packages/android-app/app/src/main/java/de/beechy/kiellive/Config.java
@@ -1,6 +1,6 @@
 package de.beechy.kiellive;
 
 public class Config {
-    public static final  String APP_HOST = "kiel.ju60.de";
+    public static final  String APP_HOST = "kiel-live.github.io";
     public static final String APP_URL = "https://" + APP_HOST;
 }
