ee5d1ab1fb78597c703616c59da69cc8b17a992e
Lukas Hass <lukas@slucky.de>
2021-12-08 20:45:29 +0100

    fix: add prettier scrollbars

--- app/src/App.vue
+++ app/src/App.vue
@@ -27,6 +27,27 @@ body {
   /* disable android pull to refresh feature */
   overflow-y: hidden;
 }
+*::-webkit-scrollbar {
+  @apply bg-transparent w-12px h-12px;
+}
+
+* {
+  scrollbar-width: thin;
+}
+
+*::-webkit-scrollbar-thumb {
+  transition: background 0.2s ease-in-out;
+  border: 3px solid transparent;
+  @apply bg-cool-gray-500 dark:bg-dark-200 rounded-full bg-clip-content;
+}
+
+*::-webkit-scrollbar-thumb:hover {
+  @apply bg-cool-gray-600 dark:bg-dark-100;
+}
+
+*::-webkit-scrollbar-corner {
+  @apply bg-transparent;
+}
 </style>
 
 <style scoped>
