03eb32634317ecf4d77d5aa09f8b64a900b99b7e
Lukas Hass <lukas@slucky.de>
2021-12-22 17:46:27 +0100

    feat: add no data message for emtpy stop arrivals

--- app/src/components/popups/BusStopPopup.vue
+++ app/src/components/popups/BusStopPopup.vue
@@ -8,24 +8,27 @@
         <i-ph-star-bold v-else @click="addFavorite(stop)" />
       </div>
     </div>
-    <div v-if="stop.arrivals?.length > 0" class="overflow-y-auto">
-      <router-link
-        v-for="arrival in stop.arrivals"
-        :key="arrival.tripId"
-        class="flex py-2 w-full not-last:border-b-1 dark:border-dark-600"
-        :to="{ name: 'map-marker', params: { markerType: 'bus', markerId: arrival.vehicleId } }"
-      >
-        <i-fa-bus class="mr-2" />
-        <span class="mr-2">{{ arrival.routeName }}</span>
-        <span class="flex-grow">{{ arrival.direction }}</span>
-        <span>{{ eta(arrival) }}</span>
-        <div class="ml-2">
-          <i-fa-solid-clock v-if="arrival.state === 'planned'" />
-          <i-fa-solid-hand-paper v-if="arrival.state === 'stopping'" />
-          <i-fa-solid-running v-if="arrival.state === 'predicted'" />
-        </div>
-      </router-link>
-    </div>
+    <template v-if="stop.arrivals">
+      <div v-if="stop.arrivals.length > 0" class="overflow-y-auto">
+        <router-link
+          v-for="arrival in stop.arrivals"
+          :key="arrival.tripId"
+          class="flex py-2 w-full not-last:border-b-1 dark:border-dark-600"
+          :to="{ name: 'map-marker', params: { markerType: 'bus', markerId: arrival.vehicleId } }"
+        >
+          <i-fa-bus class="mr-2" />
+          <span class="mr-2">{{ arrival.routeName }}</span>
+          <span class="flex-grow">{{ arrival.direction }}</span>
+          <span>{{ eta(arrival) }}</span>
+          <div class="ml-2">
+            <i-fa-solid-clock v-if="arrival.state === 'planned'" />
+            <i-fa-solid-hand-paper v-if="arrival.state === 'stopping'" />
+            <i-fa-solid-running v-if="arrival.state === 'predicted'" />
+          </div>
+        </router-link>
+      </div>
+      <NoData v-else>Hier will gerade wohl kein Manni halten.</NoData>
+    </template>
     <i-fa-solid-circle-notch v-else class="mx-auto mt-4 text-3xl animate-spin" />
   </div>
   <NoData v-else>Diese Haltestelle gibt es wohl nicht.</NoData>
