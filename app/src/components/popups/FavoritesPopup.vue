c5fc466a763b0c0dc1ee9c817c00b26c1f438493
Anton Bracke <anton@ju60.de>
2022-01-21 01:32:37 +0100

    fix scrolling in search and favorites popup, show text if no search result

--- app/src/components/popups/FavoritesPopup.vue
+++ app/src/components/popups/FavoritesPopup.vue
@@ -7,18 +7,20 @@
     <div v-if="favorites.length === 0" class="m-auto max-w-52 text-center text-xl">
       <p>Füge neue Haltestellen als Favoriten hinzu, indem du beim Öffnen der Haltestelle auf den Stern klickst.</p>
     </div>
-    <router-link
-      v-for="favorite in favorites"
-      :key="favorite.id"
-      :to="{ name: 'map-marker', params: { markerType: favorite.type, markerId: favorite.id } }"
-      class="flex py-2 not-last:border-b-1 dark:border-dark-300"
-    >
-      <i-mdi-sign-real-estate v-if="favorite.type === 'bus-stop'" class="mr-2" />
-      <!-- <i-fa-bus v-else-if="searchResult.item.type === 'bus'" class="mr-2" /> -->
-      <div class="">
-        {{ favorite.name }}
-      </div>
-    </router-link>
+    <div class="flex flex-col overflow-y-auto">
+      <router-link
+        v-for="favorite in favorites"
+        :key="favorite.id"
+        :to="{ name: 'map-marker', params: { markerType: favorite.type, markerId: favorite.id } }"
+        class="flex py-2 not-last:border-b-1 dark:border-dark-300"
+      >
+        <i-mdi-sign-real-estate v-if="favorite.type === 'bus-stop'" class="mr-2" />
+        <!-- <i-fa-bus v-else-if="searchResult.item.type === 'bus'" class="mr-2" /> -->
+        <div class="">
+          {{ favorite.name }}
+        </div>
+      </router-link>
+    </div>
   </div>
 </template>
 
--- app/src/components/popups/SearchPopup.vue
+++ app/src/components/popups/SearchPopup.vue
@@ -4,22 +4,27 @@
       <i-ph-magnifying-glass-bold />
       <span class="text-lg">Suche</span>
     </div>
-    <div v-if="searchResults.length === 0" class="m-auto max-w-52 text-center text-xl">
+    <div v-if="searchResults.length === 0 && searchInput.length < 3" class="m-auto max-w-52 text-center text-xl">
       <p>Suche nach einer Haltestelle oder einem Fahrzeug</p>
     </div>
-    <router-link
-      v-for="searchResult in searchResults"
-      :key="searchResult.refIndex"
-      :to="{ name: 'map-marker', params: { markerType: searchResult.item.type, markerId: searchResult.item.id } }"
-      class="flex py-2 not-last:border-b-1 dark:border-dark-300 max-w-full"
-      @click="$emit('update:search-input', '')"
-    >
-      <i-mdi-sign-real-estate v-if="searchResult.item.type === 'bus-stop'" class="mr-2" />
-      <!-- <i-fa-bus v-else-if="searchResult.item.type === 'bus'" class="mr-2" /> -->
-      <div class="">
-        {{ searchResult.item.name }}
-      </div>
-    </router-link>
+    <div v-else-if="searchResults.length === 0 && searchInput.length >= 3" class="m-auto max-w-52 text-center text-xl">
+      <p>Zu deiner Suche existiert anscheinend kein Eintrag.</p>
+    </div>
+    <div class="flex flex-col overflow-y-auto">
+      <router-link
+        v-for="searchResult in searchResults"
+        :key="searchResult.refIndex"
+        :to="{ name: 'map-marker', params: { markerType: searchResult.item.type, markerId: searchResult.item.id } }"
+        class="flex py-2 not-last:border-b-1 dark:border-dark-300 max-w-full"
+        @click="$emit('update:search-input', '')"
+      >
+        <i-mdi-sign-real-estate v-if="searchResult.item.type === 'bus-stop'" class="mr-2" />
+        <!-- <i-fa-bus v-else-if="searchResult.item.type === 'bus'" class="mr-2" /> -->
+        <div class="">
+          {{ searchResult.item.name }}
+        </div>
+      </router-link>
+    </div>
   </div>
 </template>
 
