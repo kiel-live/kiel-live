981b56530d0e848970ec65d2ba1441f573939ddb
Anbraten <anton@ju60.de>
2022-05-26 10:09:45 +0200

    adjust about popup border style

--- app/src/components/popups/AboutPopup.vue
+++ app/src/components/popups/AboutPopup.vue
@@ -4,22 +4,27 @@
       <i-ph-info />
       <span class="text-lg">Kiel-Live</span>
     </h1>
-    <div class="flex flex-col gap-2 mx-2 text-l overflow-y-auto">
+    <div class="flex items-start flex-col gap-2 mx-2 text-l overflow-y-auto">
       <p class="mb-2">
         Wo bleibt mein Bus? Kiel-Live zeigt aktuelle Abfahrtszeiten für alle Haltestellen und die aktuelle Position von
         deinem Bus an.
       </p>
       <p class="mb-2">Unterstütze uns indem du die App mit deinen Freunden teilst und gib uns Feedback!</p>
-      <Button href="https://www.instagram.com/kiel.live/"><i-mdi-instagram class="mr-2" />Follow @kiel.live</Button>
-      <Button href="https://github.com/kiel-live/kiel-live"><i-mdi-github class="mr-2" />Develop on Github</Button>
-      <h2 class="border-b-1 dark:border-dark-100 mt-2 font-bold">Changelog</h2>
-      <h3 class="border-b-1 dark:border-dark-100 mt-2">{{ new Date('2022-05-03').toLocaleDateString() }}</h3>
+      <Button href="https://www.instagram.com/kiel.live/" class="w-full">
+        <i-mdi-instagram class="mr-2" />Follow @kiel.live
+      </Button>
+      <Button href="https://github.com/kiel-live/kiel-live" class="w-full">
+        <i-mdi-github class="mr-2" />Develop on Github
+      </Button>
+
+      <h2 class="border-b-2 w-full dark:border-dark-100 mt-4 font-bold">Changelog</h2>
+      <h3 class="border-b-1 dark:border-gray-600 mt-2">{{ new Date('2022-05-03').toLocaleDateString() }}</h3>
       <span>Komplett neue App mit Fokus auf der Karte</span>
-      <h3 class="border-b-1 dark:border-dark-100 mt-2">{{ new Date('2020-05-13').toLocaleDateString() }}</h3>
+      <h3 class="border-b-1 dark:border-gray-600 mt-2">{{ new Date('2020-05-13').toLocaleDateString() }}</h3>
       <span>Karte mit Haltestellen und Echtzeit-Positionen aller Busse</span>
-      <h3 class="border-b-1 dark:border-dark-100 mt-2">{{ new Date('2019-06-22').toLocaleDateString() }}</h3>
+      <h3 class="border-b-1 dark:border-gray-600 mt-2">{{ new Date('2019-06-22').toLocaleDateString() }}</h3>
       <span>Anzeige von Verkehrs-Warnungen hinzugefügt</span>
-      <h3 class="border-b-1 dark:border-dark-100 mt-2">{{ new Date('2019-05-20').toLocaleDateString() }}</h3>
+      <h3 class="border-b-1 dark:border-gray-600 mt-2">{{ new Date('2019-05-20').toLocaleDateString() }}</h3>
       <span><i-fa-solid-birthday-cake class="align-text-bottom" /> Projekt gestartet</span>
     </div>
   </div>
