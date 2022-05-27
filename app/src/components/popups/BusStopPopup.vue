96815d1276386cca80b83e0d7969554dc16cd07f
Lukas Hass <lukas@slucky.de>
2022-05-27 15:38:28 +0200

    fix: scroll alerts

--- app/src/components/popups/BusStopPopup.vue
+++ app/src/components/popups/BusStopPopup.vue
@@ -9,35 +9,37 @@
       </div>
     </div>
 
-    <div v-if="stop.alerts && stop.alerts.length >= 1" class="bg-red-600 bg-opacity-50 p-2 mb-2 rounded-md">
-      <div class="flex items-center border-b-1 mb-4">
-        <i-mdi-alert class="mr-2" /><span class="font-bold">Hinweise</span>
+    <div class="overflow-y-auto">
+      <div v-if="stop.alerts && stop.alerts.length >= 1" class="bg-red-600 bg-opacity-50 p-2 mb-2 rounded-md">
+        <div class="flex items-center border-b-1 mb-4">
+          <i-mdi-alert class="mr-2" /><span class="font-bold">Hinweise</span>
+        </div>
+        <div v-for="(alert, i) in stop.alerts" :key="i" class="flex items-center">{{ alert }}</div>
       </div>
-      <div v-for="(alert, i) in stop.alerts" :key="i" class="flex items-center">{{ alert }}</div>
-    </div>
 
-    <template v-if="stop.arrivals">
-      <div v-if="stop.arrivals.length > 0" class="overflow-y-auto">
-        <router-link
-          v-for="arrival in stop.arrivals"
-          :key="arrival.tripId"
-          class="flex py-2 w-full not-last:border-b-1 dark:border-dark-300"
-          :to="{ name: 'map-marker', params: { markerType: 'bus', markerId: arrival.vehicleId } }"
-        >
-          <i-fa-bus class="mr-2" />
-          <span class="mr-2">{{ arrival.routeName }}</span>
-          <span class="flex-grow">{{ arrival.direction }}</span>
-          <span>{{ eta(arrival) }}</span>
-          <div class="ml-2">
-            <i-fa-solid-clock v-if="arrival.state === 'planned'" />
-            <i-fa-solid-hand-paper v-if="arrival.state === 'stopping'" />
-            <i-fa-solid-running v-if="arrival.state === 'predicted'" />
-          </div>
-        </router-link>
-      </div>
-      <NoData v-else>Hier will gerade wohl kein Manni halten.</NoData>
-    </template>
-    <i-fa-solid-circle-notch v-else class="mx-auto mt-4 text-3xl animate-spin" />
+      <template v-if="stop.arrivals">
+        <template v-if="stop.arrivals.length > 0">
+          <router-link
+            v-for="arrival in stop.arrivals"
+            :key="arrival.tripId"
+            class="flex py-2 w-full not-last:border-b-1 dark:border-dark-300"
+            :to="{ name: 'map-marker', params: { markerType: 'bus', markerId: arrival.vehicleId } }"
+          >
+            <i-fa-bus class="mr-2" />
+            <span class="mr-2">{{ arrival.routeName }}</span>
+            <span class="flex-grow">{{ arrival.direction }}</span>
+            <span>{{ eta(arrival) }}</span>
+            <div class="ml-2">
+              <i-fa-solid-clock v-if="arrival.state === 'planned'" />
+              <i-fa-solid-hand-paper v-if="arrival.state === 'stopping'" />
+              <i-fa-solid-running v-if="arrival.state === 'predicted'" />
+            </div>
+          </router-link>
+        </template>
+        <NoData v-else>Hier will gerade wohl kein Manni halten.</NoData>
+      </template>
+      <i-fa-solid-circle-notch v-else class="mx-auto mt-4 text-3xl animate-spin" />
+    </div>
   </div>
   <NoData v-else>
     Diese Haltestelle gibt es wohl nicht.
