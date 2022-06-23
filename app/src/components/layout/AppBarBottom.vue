01586570b9f47c321bc65bee4a8bcc503a3999c1
Anbraten <anton@ju60.de>
2022-06-23 07:23:54 +0200

    improve style, add translations

--- app/src/components/layout/AppBarBottom.vue
+++ app/src/components/layout/AppBarBottom.vue
@@ -1,31 +1,39 @@
 <template>
-  <div class="flex w-full justify-around border-t-1 dark:border-gray-700 drop-shadow">
+  <div
+    class="flex w-full justify-around border-t-1 border-gray-200 dark:border-gray-700 bg-gray-100 dark:bg-dark-400 dark:text-gray-300 drop-shadow"
+  >
     <router-link
       :to="{ name: 'home' }"
       class="flex flex-col items-center w-1/3 p-2"
       :class="{ 'text-blue-400': $route.name !== 'favorites' && $route.name !== 'settings' }"
-      aria-label="Karte"
+      :aria-label="t('map')"
     >
-      <i-carbon-map class="w-7 h-7 mb-1" />
-      <span class="mt-auto text-sm">Karte</span>
+      <i-carbon-map class="w-6 h-6 mb-1" />
+      <span class="mt-auto text-xs">{{ t('map') }}</span>
     </router-link>
     <router-link
       :to="{ name: 'favorites' }"
       class="flex flex-col items-center w-1/3 p-2"
       :class="{ 'text-blue-400': $route.name === 'favorites' }"
-      aria-label="Favoriten"
+      :aria-label="t('favorites')"
     >
-      <i-ph-star-fill class="w-7 h-7 mb-1" />
-      <span class="mt-auto text-sm">Favoriten</span>
+      <i-ph-star-fill class="w-6 h-6 mb-1" />
+      <span class="mt-auto text-xs">{{ t('favorites') }}</span>
     </router-link>
     <router-link
       :to="{ name: 'settings' }"
       class="flex flex-col items-center w-1/3 p-2"
       :class="{ 'text-blue-400': $route.name === 'settings' }"
-      aria-label="Über Kiel-Live"
+      :aria-label="t('settings')"
     >
-      <i-ic-baseline-settings class="w-7 h-7 mb-1" />
-      <span class="mt-auto text-sm">Einstellungen</span>
+      <i-ic-baseline-settings class="w-6 h-6 mb-1" />
+      <span class="mt-auto text-xs">{{ t('settings') }}</span>
     </router-link>
   </div>
 </template>
+
+<script lang="ts" setup>
+import { useI18n } from 'vue-i18n';
+
+const { t } = useI18n();
+</script>
