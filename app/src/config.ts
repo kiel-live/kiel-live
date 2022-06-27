3969fa4a4d926e879acf45f52b7d648f03649d06
Anbraten <anton@ju60.de>
2022-06-27 19:15:58 +0000

    move version back again

--- app/src/config.ts
+++ app/src/config.ts
@@ -1,5 +1,4 @@
 export const feedbackMail = atob('YW5kcm9pZEBqdTYwLmRl'); // email as base64
-export const version = '2.0.0'; // TODO: get from package json :thinking:?
 export const buildDate = import.meta.env.VITE_BUILD_DATE;
 export const analyticsUrl = 'https://boomerang.ju60.de/share/z8KAHmGY/Kiel%20Live';
 export const tileServer = 'https://tiles.slucky.de';
