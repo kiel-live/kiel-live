54469a7ae153a8a26d48e29eec96ec66ba2634e8
Anbraten <anton@ju60.de>
2022-07-27 05:40:04 +0000

    fix: use new tiles server

--- app/src/config.ts
+++ app/src/config.ts
@@ -1,7 +1,7 @@
 export const feedbackMail = atob('YW5kcm9pZEBqdTYwLmRl'); // email as base64
 export const buildDate = import.meta.env.VITE_BUILD_DATE;
 export const analyticsUrl = 'https://boomerang.ju60.de/share/z8KAHmGY/Kiel%20Live';
-export const tileServer = 'https://tiles.slucky.de';
+export const tileServer = 'https://tiles.ju60.de';
 export const darkMapStyle = `${tileServer}/styles/gray-matter/style.json`;
 export const brightMapStyle = `${tileServer}/styles/bright-matter/style.json`;
 export const natsServerUrl = import.meta.env.VITE_NATS_URL;
