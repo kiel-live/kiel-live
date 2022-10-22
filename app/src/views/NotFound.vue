8317c64d77796242591386c3c964d038c3d85963
Lukas Hass <lukas@slucky.de>
2022-10-22 12:10:22 +0200

    improve formatting

--- app/src/views/NotFound.vue
+++ app/src/views/NotFound.vue
@@ -4,9 +4,9 @@
       <p class="text-2xl mb-8">{{ t('404_error') }}</p>
       <i18n-t keypath="404_back_to" tag="span">
         <template #link>
-          <router-link class="text-blue-700 dark:text-blue-400" replace :to="{ name: 'home' }">{{
-            t('home')
-          }}</router-link>
+          <router-link class="text-blue-700 dark:text-blue-400" replace :to="{ name: 'home' }">
+            {{ t('home') }}
+          </router-link>
         </template>
       </i18n-t>
     </div>
