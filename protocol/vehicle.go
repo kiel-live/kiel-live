4a640676f85df941b80eeed99f9b9e842eb18bfb
Anbraten <anton@ju60.de>
2023-01-02 13:47:55 +0100

    improve protocol types

--- protocol/vehicle.go
+++ protocol/vehicle.go
@@ -1,24 +1,27 @@
 package protocol
 
 // Type of a vehicle
+
+type VehicleType string
+
 const (
-	VehicleTypeBus      = "bus"
-	VehicleTypeBike     = "bike"
-	VehicleTypeCar      = "car"
-	VehicleTypeEScooter = "e-scooter"
-	VehicleTypeFerry    = "ferry"
-	VehicleTypeTrain    = "train"
-	VehicleTypeSubway   = "subway"
+	VehicleTypeBus      VehicleType = "bus"
+	VehicleTypeBike     VehicleType = "bike"
+	VehicleTypeCar      VehicleType = "car"
+	VehicleTypeEScooter VehicleType = "e-scooter"
+	VehicleTypeFerry    VehicleType = "ferry"
+	VehicleTypeTrain    VehicleType = "train"
+	VehicleTypeSubway   VehicleType = "subway"
 )
 
 // Vehicle can be of a specific type (exp. bus, bike).
 type Vehicle struct {
-	ID       string   `json:"id"`
-	Provider string   `json:"provider"`
-	Name     string   `json:"name"`
-	Type     string   `json:"type"` // use VehicleType...
-	State    string   `json:"state"`
-	Battery  string   `json:"battery"` // in percent
-	Location Location `json:"location"`
-	TripID   string   `json:"tripId"`
+	ID       string      `json:"id"`
+	Provider string      `json:"provider"`
+	Name     string      `json:"name"`
+	Type     VehicleType `json:"type"`
+	State    string      `json:"state"`
+	Battery  string      `json:"battery"` // in percent
+	Location Location    `json:"location"`
+	TripID   string      `json:"tripId"`
 }
