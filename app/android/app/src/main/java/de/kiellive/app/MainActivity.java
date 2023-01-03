56d51aaac294667ca0e321b9df20c17f6a278254
Lukas Hass <lukas@slucky.de>
2023-01-03 18:34:37 +0100

    chore: add capacitor build

---
+++ app/android/app/src/main/java/de/kiellive/app/MainActivity.java
@@ -0,0 +1,5 @@
+package de.kiellive.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ app/capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'de.kiellive.app',
+  appName: 'Kiel Live',
+  webDir: 'dist',
+  bundledWebRuntime: false,
+};
+
+export default config;
