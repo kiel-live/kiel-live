52934f56c10f6f8ccacb6d398ef1774aecf6df60
Lukas Hass <lukas@slucky.de>
2023-01-22 20:21:45 +0100

    replace reload prompt with button in app bar

--- app/src/components/layout/AppBar.vue
+++ app/src/components/layout/AppBar.vue
@@ -1,11 +1,11 @@
 <template>
   <div
-    class="absolute top-0 left-0 right-0 mx-2 mt-2 h-12 flex rounded-md py-4 items-center justify-between bg-white border-1 border-gray-200 shadow-xl z-20 md:transform md:-translate-x-1/2 md:right-auto md:left-1/2 md:w-96 dark:bg-dark-400 dark:text-gray-300 dark:border-dark-800"
+    class="absolute top-0 left-0 right-0 mx-2 mt-2 h-12 flex rounded-md py-1 pr-1 gap-x-1 items-center justify-between bg-white border-1 border-gray-200 shadow-xl z-20 md:transform md:-translate-x-1/2 md:right-auto md:left-1/2 md:w-96 dark:bg-dark-400 dark:text-gray-300 dark:border-dark-800"
   >
     <router-link :to="{ name: 'home' }" class="p-2">
       <img :alt="t('logo_alt')" src="../../assets/logo.png" class="w-6 h-6" />
     </router-link>
-    <div v-if="isConnected" class="flex flex-grow mr-2">
+    <div v-if="isConnected" class="flex flex-grow h-full">
       <input
         :value="internalSearchInput"
         type="text"
@@ -18,19 +18,25 @@
         @click="$router.push({ name: 'search' })"
       />
     </div>
-    <div v-else class="flex space-x-2 mr-2 items-center">
+    <div v-else class="flex gap-x-2 mr-2 items-center">
       <span>{{ t('no_connection') }}</span>
       <i-ic-baseline-cloud-off class="text-red-600" />
     </div>
+    <Button v-if="!needRefresh" class="h-full gap-x-1" @click="updateServiceWorker(true)">
+      <i-ic-sharp-refresh />
+      <span>{{ t('update') }}</span>
+    </Button>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { useRegisterSW } from 'virtual:pwa-register/vue';
 import { computed, toRef } from 'vue';
 import { useI18n } from 'vue-i18n';
 import { useRoute, useRouter } from 'vue-router';
 
 import { isConnected } from '~/api';
+import Button from '~/components/atomic/Button.vue';
 
 const props = defineProps<{
   searchInput: string;
@@ -43,6 +49,7 @@ const emit = defineEmits<{
 const { t } = useI18n();
 const route = useRoute();
 const router = useRouter();
+const { needRefresh, updateServiceWorker } = useRegisterSW();
 
 const searchInput = toRef(props, 'searchInput');
 const internalSearchInput = computed({
