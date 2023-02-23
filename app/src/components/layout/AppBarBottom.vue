c7ca32b4983b1cb7092a815024c1bfe9ece7abd5
Anbraten <anton@ju60.de>
2023-02-23 14:47:42 +0100

    refactor: use red buttons for bottom navigation (#212)
    Co-authored-by: Lukas Hass <lukas@slucky.de>

--- app/src/components/layout/AppBarBottom.vue
+++ app/src/components/layout/AppBarBottom.vue
@@ -7,7 +7,7 @@
         v-if="liteMode"
         :to="{ name: 'search' }"
         class="flex flex-col items-center w-1/3 p-2"
-        :class="{ 'text-blue-700 dark:text-blue-400': activeArea === 'map' }"
+        :class="{ 'text-red-700 dark:text-red-500': activeArea === 'map' }"
         :aria-label="t('search')"
       >
         <i-ph-magnifying-glass-bold class="w-6 h-6 mb-1" />
@@ -17,7 +17,7 @@
         v-else
         :to="{ name: 'home' }"
         class="flex flex-col items-center w-1/3 p-2"
-        :class="{ 'text-blue-700 dark:text-blue-400': activeArea === 'map' }"
+        :class="{ 'text-red-700 dark:text-red-500': activeArea === 'map' }"
         :aria-label="t('map')"
       >
         <i-carbon-map class="w-6 h-6 mb-1" />
@@ -26,7 +26,7 @@
       <router-link
         :to="{ name: 'favorites' }"
         class="flex flex-col items-center w-1/3 p-2"
-        :class="{ 'text-blue-700 dark:text-blue-400': activeArea === 'favorites' }"
+        :class="{ 'text-red-700 dark:text-red-500': activeArea === 'favorites' }"
         :aria-label="t('favorites')"
       >
         <i-ph-star-fill class="w-6 h-6 mb-1" />
@@ -35,7 +35,7 @@
       <router-link
         :to="{ name: 'settings-about' }"
         class="flex flex-col items-center w-1/3 p-2"
-        :class="{ 'text-blue-700 dark:text-blue-400': activeArea === 'settings' }"
+        :class="{ 'text-red-700 dark:text-red-500': activeArea === 'settings' }"
         :aria-label="t('settings')"
       >
         <i-ic-baseline-settings class="w-6 h-6 mb-1" />
