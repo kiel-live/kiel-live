8a2fceff8d7741a81a834e4c216a198f726864a2
Lukas Hass <lukas@slucky.de>
2023-03-23 11:58:49 +0100

    add setting

--- app/src/compositions/useUserSettings.ts
+++ app/src/compositions/useUserSettings.ts
@@ -4,6 +4,7 @@ export const localStoragePrefix = 'kiel_live';
 
 const userSettings = {
   liteMode: useStorage(`${localStoragePrefix}.lite`, false),
+  geolocationAllowed: useStorage(`${localStoragePrefix}.geolocationAllowed`, false),
 };
 
 export function useUserSettings() {
