3d399a559d115d10b197bcb8686a0598181d6008
Anbraten <anton@ju60.de>
2024-08-14 13:58:57 +0200

    adjust types

--- app/src/api/types/stop.ts
+++ app/src/api/types/stop.ts
@@ -6,10 +6,10 @@ export type Stop = {
   id: string;
   provider: string;
   name: string;
-  type: ArrivalType; // deprecated: use arrivals[].type instead
+  type: ArrivalType; // deprecated: use arrivals[].type or vehicles[].type instead
   routes: string[] | null; // list of routes using this stop
   alerts: string[] | null; // general alerts for this stop
-  arrivals: StopArrival[] | null;
+  arrivals?: StopArrival[];
   location: GpsLocation;
-  vehicles: Vehicle[];
+  vehicles?: Vehicle[];
 };
--- protocol/arrival.go
+++ protocol/arrival.go
@@ -12,28 +12,28 @@ const (
 )
 
 // Type of a stop
-type StopArrivalType string
+type ArrivalType string
 
 const (
-	StopTypeBusStop     StopArrivalType = "bus-stop"
-	StopTypeParkingSpot StopArrivalType = "parking-spot"
-	StopTypeFerryStop   StopArrivalType = "ferry-stop"
-	StopTypeTrainStop   StopArrivalType = "train-stop"
-	StopTypeSubwayStop  StopArrivalType = "subway-stop"
+	StopTypeBusStop     ArrivalType = "bus-stop"
+	StopTypeParkingSpot ArrivalType = "parking-spot"
+	StopTypeFerryStop   ArrivalType = "ferry-stop"
+	StopTypeTrainStop   ArrivalType = "train-stop"
+	StopTypeSubwayStop  ArrivalType = "subway-stop"
 )
 
 type StopArrival struct {
-	Name      string          `json:"name"`
-	Type      StopArrivalType `json:"type"`
-	VehicleID string          `json:"vehicleId"`
-	TripID    string          `json:"tripId"`
-	RouteID   string          `json:"routeId"`
-	RouteName string          `json:"routeName"`
-	Direction string          `json:"direction"`
-	State     ArrivalState    `json:"state"`
-	Planned   string          `json:"planned"`
-	ETA       int             `json:"eta"` // in seconds
-	Platform  string          `json:"platform"`
+	Name      string       `json:"name"`
+	Type      ArrivalType  `json:"type"`
+	VehicleID string       `json:"vehicleId"`
+	TripID    string       `json:"tripId"`
+	RouteID   string       `json:"routeId"`
+	RouteName string       `json:"routeName"`
+	Direction string       `json:"direction"`
+	State     ArrivalState `json:"state"`
+	Planned   string       `json:"planned"`
+	ETA       int          `json:"eta"` // in seconds
+	Platform  string       `json:"platform"`
 }
 
 type TripArrival struct {
