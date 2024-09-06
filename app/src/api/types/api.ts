5f81499439177d51676359a8a229b4cad9f378c3
Anbraten <anton@ju60.de>
2024-09-06 21:21:31 +0200

    remove comments

--- app/src/api/types/api.ts
+++ app/src/api/types/api.ts
@@ -6,36 +6,26 @@ import { Trip } from './trip';
 import { Vehicle } from './vehicle';
 
 export interface Api {
-  // getStops(bounds: Bounds): Stop[];
-  // onStopsUpdated(bounds: Bounds, cb: (stop: Stop) => void): void;
   useStops(bounds: Ref<Bounds>): { stops: Ref<Stop[]>; loading: Ref<boolean>; unsubscribe: () => void | Promise<void> };
 
-  // getVehicles(bounds: Bounds): Vehicle[];
-  // onVehiclesUpdated(bounds: Bounds, cb: (vehicle: Vehicle) => void): void;
   useVehicles(bounds: Ref<Bounds>): {
     vehicles: Ref<Vehicle[]>;
     loading: Ref<boolean>;
     unsubscribe: () => void | Promise<void>;
   };
 
-  // getStop(stopId: string): Stop | null;
-  // onStopUpdated(stopId: string, cb: (stop: Stop) => void): void;
   useStop(stopId: Ref<string | undefined>): {
     stop: Ref<Stop | null>;
     loading: Ref<boolean>;
     unsubscribe: () => void | Promise<void>;
   };
 
-  // getVehicle(vehicleId: string): Vehicle | null;
-  // onVehicleUpdated(vehicleId: string, cb: (vehicle: Vehicle) => void): void;
   useVehicle(vehicleId: Ref<string | undefined>): {
     vehicle: Ref<Vehicle | null>;
     loading: Ref<boolean>;
     unsubscribe: () => void | Promise<void>;
   };
 
-  // getTrip(tripId: string): Trip | null;
-  // onTripUpdated(tripId: string, cb: (trip: Trip) => void): void;
   useTrip(tripId: Ref<string | undefined>): {
     trip: Ref<Trip | null>;
     loading: Ref<boolean>;
