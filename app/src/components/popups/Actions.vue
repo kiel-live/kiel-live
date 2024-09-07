507fd3d500b6330009b4f954f918ce1238940881
Anbraten <anton@ju60.de>
2024-09-07 14:18:12 +0200

    fix style

--- app/src/components/popups/Actions.vue
+++ app/src/components/popups/Actions.vue
@@ -2,24 +2,19 @@
   <div v-if="actions.length > 0 && checkFeatureFlag('vehicle_stop_actions').value" class="flex mt-2 overflow-x-auto">
     <div class="inline-flex w-min gap-2 pb-4">
       <template v-for="action in actions" :key="action.url">
-        <a :href="action.url" target="_blank" rel="noopener noreferrer" class="flex flex-shrink-0">
-          <button
-            type="button"
-            class="border border-gray-300 dark:border-dark-100 text-blue-600 rounded-full px-4 py-1 dark:text-white flex items-center gap-1"
-          >
-            <template v-if="action.type === 'navigate-to'">
-              <i-ic-baseline-directions />
-              <span>{{ t('navigate_to') }}</span>
-            </template>
-            <template v-else-if="action.type === 'rent'">
-              <i-ic-baseline-play-arrow />
-              <span>{{ t('rent_vehicle') }}</span>
-            </template>
-            <template v-else>
-              <span>{{ action.name }}</span>
-            </template>
-          </button>
-        </a>
+        <Button :href="action.url" rounded class="gap-2 flex-shrink-0 px-4 py-2">
+          <template v-if="action.type === 'navigate-to'">
+            <i-ic-baseline-directions />
+            <span>{{ t('navigate_to') }}</span>
+          </template>
+          <template v-else-if="action.type === 'rent'">
+            <i-ic-baseline-play-arrow />
+            <span>{{ t('rent_vehicle') }}</span>
+          </template>
+          <template v-else>
+            <span>{{ action.name }}</span>
+          </template>
+        </Button>
       </template>
     </div>
   </div>
@@ -29,6 +24,7 @@
 import { useI18n } from 'vue-i18n';
 
 import { Action } from '~/api/types/action';
+import Button from '~/components/atomic/Button.vue';
 import { useFeatureFlags } from '~/compositions/useFeatureFlags';
 
 const { t } = useI18n();
