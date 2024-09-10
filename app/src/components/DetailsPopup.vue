238ad0d1fb4a3ad8356e9b6b28f526d71a0aa270
Anbraten <anton@ju60.de>
2024-09-10 08:29:43 +0200

    tweaks

--- app/src/components/DetailsPopup.vue
+++ app/src/components/DetailsPopup.vue
@@ -10,7 +10,7 @@
   >
     <DrawerPortal>
       <DrawerContent
-        class="fixed flex flex-col bg-gray-100 dark:bg-dark-400 dark:text-gray-300 dark:border-dark-800 rounded-t-[10px] max-h-[97%] bottom-0 left-0 right-0 h-full mx-[-1px] z-10"
+        class="fixed flex flex-col bg-gray-100 dark:bg-dark-400 dark:text-gray-300 dark:border-dark-800 rounded-t-[2rem] max-h-[calc(100%-64px)] bottom-0 left-0 right-0 h-full mx-[-1px] z-10"
       >
         <div class="p-4 pb-0 pt-2" :class="snap === 1 ? 'overflow-y-auto' : 'overflow-hidden'">
           <div v-if="!disableResize" class="w-full -mt-4 pt-4 pb-4 md:hidden">
@@ -39,8 +39,8 @@ defineEmits<{
 const open = defineModel<boolean>('open', { required: true });
 
 const snapPoints = computed(() => {
-  if (props.size === '1/2') return [0.2, 0.5];
-  return [0.2, 0.5, 1];
+  if (props.size === '1/2') return ['200px', 0.5];
+  return ['200px', 0.5, 1];
 });
 
 const snap = ref<number | string | null>(0.5);
