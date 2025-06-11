fbc34d680bb31609d13b3d82c69f25ebe713baff
Anbraten <anton@ju60.de>
2025-06-11 08:17:45 +0200

    enhance

--- app/src/api/http.ts
+++ app/src/api/http.ts
@@ -1,30 +1,73 @@
 import type { Ref } from 'vue';
-import type { Api, Bounds, Models, Stop, Trip, Vehicle } from '~/api/types';
+import type { Api, Bounds, Models as Model, Stop, Trip, Vehicle } from '~/api/types';
+import { s2 } from 's2js';
 
 import { computed, ref, watch } from 'vue';
 
+interface WebsocketMessage<T = unknown> {
+  topic: string;
+  action?: 'updated' | 'deleted';
+  data?: T;
+  // TODO: we could extend websocket message and models with a timestamp and only update using newer messages
+}
+
+type Store<T extends Model> = Ref<Map<string, T>>;
+
+function isModel<T extends Model>(data: unknown): data is T {
+  return data !== null && typeof data === 'object' && 'id' in data;
+}
+
+function getBoundsCellIds(bounds: Bounds): string[] {
+  const cellIds: string[] = [];
+  // const swCell = s2.cellid.latLngToCellId(bounds.sw.lat, bounds.sw.lng, 15);
+  // const neCell = s2.latLngToCellId(bounds.ne.lat, bounds.ne.lng, 15);
+
+  // // Iterate over the cells in the bounding box
+  // for (let lat = swCell.lat; lat <= neCell.lat; lat++) {
+  //   for (let lng = swCell.lng; lng <= neCell.lng; lng++) {
+  //     cellIds.push(s2.cellIdToString(s2.latLngToCellId(lat, lng, 15)));
+  //   }
+  // }
+
+  return cellIds;
+}
+
 export class HttpApi implements Api {
   isConnected = ref(false);
+  ws: WebSocket;
 
+  private topics = new Map<string, Store<Model>>();
   private vehicles = ref<Map<string, Vehicle>>(new Map());
-
   private stops = ref<Map<string, Stop>>(new Map());
-
   private trips = ref<Map<string, Trip>>(new Map());
 
-  constructor(autoLoad = true) {
-    if (autoLoad) {
-      void this.load();
-    }
+  constructor() {
+    this.ws = new WebSocket('ws://localhost:8080/ws'); // TODO: replace with proper reconnecting websocket
+
+    this.ws.addEventListener('message', (event) => {
+      const message: WebsocketMessage = JSON.parse(event.data);
+      const topic = message.topic;
+
+      if (isModel(message.data)) {
+        const store = this.topics.get(topic);
+        if (!store) {
+          console.warn(`No store found for topic: ${topic}. Probably failed to unsubscribe from topic.`);
+          return;
+        }
+
+        if (message.action === 'updated') {
+          store.value.set(message.data.id, message.data);
+        }
+
+        if (message.action === 'deleted') {
+          store.value.delete(message.data.id);
+        }
+      }
+    });
   }
 
-  async load() {
-    // Validate http endpoint
-    this.isConnected.value = true;
-  }
-
-  private async fetch<T>(url: string): Promise<T> {
-    const response = await fetch(url);
+  private async fetch<T>(url: string, options?: globalThis.RequestInit): Promise<T> {
+    const response = await fetch(url, options);
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
@@ -32,177 +75,152 @@ export class HttpApi implements Api {
     return response.json() as Promise<T>;
   }
 
-  private async subscribe<T extends Models>(subject: string, store: Ref<Map<string, T>>) {
-    // TODO: Implement the logic to subscribe to the subject
+  private async subscribe<T extends Model>(topic: string, store: Ref<Map<string, T>>) {
+    this.ws.send(JSON.stringify({ action: 'subscribe', subject: topic }));
+    this.topics.set(topic, store);
   }
 
-  private async unsubscribe(subject: string) {
-    // TODO: Implement the logic to unsubscribe from the subject
+  private async unsubscribe(topic: string) {
+    this.ws.send(JSON.stringify({ action: 'unsubscribe', subject: topic }));
+    this.topics.delete(topic);
   }
 
-  useStops() {
-    const loading = ref(true);
+  private useItems<T extends Model>(itemType: string, bounds: Ref<Bounds>, store: Ref<Map<string, T>>) {
+    // TODO: in case we have the same item-type + id combination, we should proxy the existing query
+    const loading = ref(false);
 
-    async function loadStops(this: HttpApi) {
+    async function loadItems(this: HttpApi, cellIds: string[]) {
+      // TODO: in case loadItem is called multiple times, only the last response should be used
+      if (loading.value === true) {
+        return;
+      }
+
       loading.value = true;
-      const stops = await this.fetch<Stop[]>(`/api/stops`);
-      stops.forEach((stop) => {
-        this.stops.value.set(stop.id, stop);
+      const items = await this.fetch<T[]>(`/api/${itemType}?cells=${cellIds.join(',')}`);
+      items.forEach((item) => {
+        store.value.set(item.id, item);
       });
       loading.value = false;
     }
 
-    void this.subscribe(`data.map.stop.>`, this.stops);
-    void loadStops.call(this);
+    watch(
+      bounds,
+      async (newBounds, oldBounds) => {
+        const newCellIds = getBoundsCellIds(newBounds);
+        const oldCellIds = oldBounds ? getBoundsCellIds(oldBounds) : [];
 
-    // TODO: watch bounds
+        // Load the complete set of items for the new bounds
+        await loadItems.call(this, newCellIds);
+
+        const addedCellIds = newCellIds.filter((id) => !oldCellIds.includes(id));
+        for (const cellId of addedCellIds) {
+          await this.subscribe(`map.${itemType}:${cellId}`, store);
+        }
+
+        const removedCellIds = oldCellIds.filter((id) => !newCellIds.includes(id));
+        for (const cellId of removedCellIds) {
+          await this.unsubscribe(`map.${itemType}:${cellId}`);
+        }
+      },
+      { immediate: true },
+    );
 
     return {
-      stops: computed(() => Array.from(this.stops.value.values())),
+      items: computed(() => Array.from(store.value.values())),
       loading,
       unsubscribe: async () => {
-        await this.unsubscribe(`data.map.stop.>`);
+        const cellIds = getBoundsCellIds(bounds.value);
+        for (const cellId of cellIds) {
+          await this.unsubscribe(`map.${itemType}:${cellId}`);
+        }
       },
     };
   }
 
-  useVehicles() {
-    const loading = ref(true);
+  private useItem<T extends Model>(itemType: string, id: Ref<string | undefined>, store: Ref<Map<string, T>>) {
+    // TODO: in case we have the same item-type + id combination, we should proxy the existing query
+    const loading = ref(false);
 
-    async function loadVehicles(this: HttpApi) {
+    async function loadItem(this: HttpApi, itemId: string | undefined) {
+      // TODO: in case loadItem is called multiple times, only the last response should be used
+      // TODO: we could extend websocket message and models with a timestamp and only update using newer messages
+      if (!itemId || loading.value === true) {
+        return;
+      }
+
       loading.value = true;
-      const vehicles = await this.fetch<Vehicle[]>(`/api/vehicles`);
-      vehicles.forEach((vehicle) => {
-        this.vehicles.value.set(vehicle.id, vehicle);
-      });
+      const item = await this.fetch<T>(`/api/${itemType}/${itemId}`);
+      store.value.set(itemId, item);
       loading.value = false;
     }
 
-    void this.subscribe(`data.map.vehicle.>`, this.vehicles);
-    void loadVehicles.call(this);
-
-    // TODO: watch bounds
+    watch(
+      id,
+      async (newId, oldId) => {
+        if (oldId) {
+          await this.unsubscribe(`${itemType}:${oldId}`);
+        }
+        if (newId) {
+          await this.subscribe(`${itemType}:${newId}`, store);
+          await loadItem.call(this, newId);
+        }
+      },
+      { immediate: true },
+    );
 
     return {
-      vehicles: computed(() => Array.from(this.vehicles.value.values())),
+      item: computed(() => (id.value ? (store.value.get(id.value) ?? null) : null)),
       loading,
       unsubscribe: async () => {
-        await this.unsubscribe(`data.map.vehicle.>`);
+        await this.unsubscribe(`${itemType}.${id.value}`);
       },
+    };
+  }
+
+  useStops(bounds: Ref<Bounds>) {
+    const store = ref<Map<string, Stop>>(new Map());
+    const { items, loading, unsubscribe } = this.useItems<Stop>('stops', bounds, store);
+    return {
+      stops: items,
+      loading,
+      unsubscribe,
+    };
+  }
+
+  useVehicles(bounds: Ref<Bounds>) {
+    const store = ref<Map<string, Vehicle>>(new Map());
+    const { items, loading, unsubscribe } = this.useItems<Vehicle>('vehicles', bounds, store);
+    return {
+      vehicles: items,
+      loading,
+      unsubscribe,
     };
   }
 
   useStop(stopId: Ref<string | undefined>) {
-    const loading = ref(true);
-
-    async function loadStop(this: HttpApi, id: string) {
-      loading.value = true;
-      if (!id) {
-        loading.value = false;
-        return;
-      }
-
-      const stop = await this.fetch<Stop>(`/api/stops/${id}`);
-      this.stops.value.set(stop.id, stop);
-      loading.value = false;
-    }
-
-    watch(
-      stopId,
-      async (newId, oldId) => {
-        if (oldId) {
-          await this.unsubscribe(`data.map.stop.${oldId}`);
-        }
-        if (newId) {
-          await this.subscribe(`data.map.stop.${newId}`, this.stops);
-          await loadStop.call(this, newId);
-        }
-      },
-      { immediate: true },
-    );
-
+    const { item, loading, unsubscribe } = this.useItem<Stop>('stops', stopId, this.stops);
     return {
-      stop: computed(() => (stopId.value ? (this.stops.value.get(stopId.value) ?? null) : null)),
+      stop: item,
       loading,
-      unsubscribe: async () => {
-        await this.unsubscribe(`data.map.stop.${stopId.value}`);
-      },
+      unsubscribe,
     };
   }
 
   useVehicle(vehicleId: Ref<string | undefined>) {
-    const loading = ref(true);
-
-    async function loadVehicle(this: HttpApi, id: string) {
-      loading.value = true;
-      if (!id) {
-        loading.value = false;
-        return;
-      }
-
-      const vehicle = await this.fetch<Vehicle>(`/api/vehicles/${id}`);
-      this.vehicles.value.set(vehicle.id, vehicle);
-      loading.value = false;
-    }
-
-    watch(
-      vehicleId,
-      async (newId, oldId) => {
-        if (oldId) {
-          await this.unsubscribe(`data.map.vehicle.${oldId}`);
-        }
-        if (newId) {
-          await this.subscribe(`data.map.vehicle.${newId}`, this.vehicles);
-          await loadVehicle.call(this, newId);
-        }
-      },
-      { immediate: true },
-    );
-
+    const { item, loading, unsubscribe } = this.useItem<Vehicle>('vehicles', vehicleId, this.vehicles);
     return {
-      vehicle: computed(() => (vehicleId.value ? (this.vehicles.value.get(vehicleId.value) ?? null) : null)),
+      vehicle: item,
       loading,
-      unsubscribe: async () => {
-        await this.unsubscribe(`data.map.vehicle.${vehicleId.value}`);
-      },
+      unsubscribe,
     };
   }
 
   useTrip(tripId: Ref<string | undefined>) {
-    const loading = ref(true);
-
-    async function loadTrip(this: HttpApi, id: string) {
-      loading.value = true;
-      if (!id) {
-        loading.value = false;
-        return;
-      }
-
-      const trip = await this.fetch<Trip>(`/api/trips/${id}`);
-      this.trips.value.set(trip.id, trip);
-      loading.value = false;
-    }
-
-    watch(
-      tripId,
-      async (newId, oldId) => {
-        if (oldId) {
-          await this.unsubscribe(`data.map.trip.${oldId}`);
-        }
-        if (newId) {
-          await this.subscribe(`data.map.trip.${newId}`, this.trips);
-          await loadTrip.call(this, newId);
-        }
-      },
-      { immediate: true },
-    );
-
+    const { item, loading, unsubscribe } = this.useItem<Trip>('trips', tripId, this.trips);
     return {
-      trip: computed(() => (tripId.value ? (this.trips.value.get(tripId.value) ?? null) : null)),
+      trip: item,
       loading,
-      unsubscribe: async () => {
-        await this.unsubscribe(`data.map.trip.${tripId.value}`);
-      },
+      unsubscribe,
     };
   }
 
--- hub/hub/hub.go
+++ hub/hub/hub.go
@@ -52,6 +52,7 @@ func (h *Hub) Run() {
 			h.mu.Lock()
 			h.clients[client] = make(map[string]struct{}) // Initialize empty set of topics
 			h.mu.Unlock()
+			h.sendStats()
 			log.Println("Client registered")
 		case client := <-h.unregister:
 			h.mu.Lock()
@@ -60,6 +61,7 @@ func (h *Hub) Run() {
 				log.Println("Client unregistered")
 			}
 			h.mu.Unlock()
+			h.sendStats()
 		case req := <-h.subscribe:
 			h.mu.Lock()
 			if clientTopics, ok := h.clients[req.client]; ok {
@@ -69,6 +71,7 @@ func (h *Hub) Run() {
 				log.Printf("Failed to subscribe: client not registered: %v", req.client)
 			}
 			h.mu.Unlock()
+			h.sendTopicList()
 		case req := <-h.unsubscribe:
 			h.mu.Lock()
 			if clientTopics, ok := h.clients[req.client]; ok {
@@ -76,6 +79,7 @@ func (h *Hub) Run() {
 				log.Printf("Client unsubscribed from topic: %s", req.topic)
 			}
 			h.mu.Unlock()
+			h.sendTopicList()
 		case message := <-h.broadcast:
 			h.mu.Lock()
 			for client, topics := range h.clients {
@@ -120,4 +124,34 @@ func (h *Hub) BroadcastMessage(topic string, action string, data any) {
 		Action: action,
 		Data:   data,
 	}
+}
+
+func (h *Hub) sendTopicList() {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+
+	topicsMap := map[string]struct{}{}
+	for client := range h.clients {
+		for topic := range h.clients[client] {
+			topicsMap[topic] = struct{}{}
+		}
+	}
+
+	topicsList := make([]string, 0, len(topicsMap))
+	for topic := range topicsMap {
+		topicsList = append(topicsList, topic)
+	}
+
+	h.BroadcastMessage("system.topics", "update", topicsList)
+}
+
+func (h *Hub) sendStats() {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+
+	stats := map[string]int{
+		"clients": len(h.clients),
+	}
+
+	h.BroadcastMessage("system.stats", "update", stats)
 }
