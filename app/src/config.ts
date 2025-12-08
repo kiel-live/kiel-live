fcd5015b6a9335434d5301f89be5a4a63db53a93
Anbraten <anton@ju60.de>
2025-12-08 20:54:12 +0100

    add map styles again

--- app/src/config.ts
+++ app/src/config.ts
@@ -3,9 +3,8 @@ import type { LngLatBoundsLike } from 'maplibre-gl';
 export const feedbackMail = atob('YW5kcm9pZEBqdTYwLmRl'); // email as base64
 export const buildDate = import.meta.env.VITE_BUILD_DATE;
 export const analyticsUrl = 'https://boomerang.ju60.de/share/z8KAHmGY/Kiel%20Live';
-const tileServer = 'https://tiles.ju60.de/styles/v2';
-export const darkMapStyle = `${tileServer}/dark.json`;
-export const lightMapStyle = `${tileServer}/light.json`;
+export const darkMapStyle = `/map-styles/dark.json`;
+export const lightMapStyle = '/map-styles/light.json';
 export const natsServerUrl = import.meta.env.VITE_NATS_URL;
 export const DEBUG = (globalThis?.window as { DEBUG?: boolean })?.DEBUG || import.meta.env.DEV;
 export const mapMaxBounds: LngLatBoundsLike = [4, 46, 16, 57]; // [west, south, east, north]
