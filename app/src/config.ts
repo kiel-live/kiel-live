5b7e02a1e508192d886f1bb67d7eb6421ea389db
Anbraten <anton@ju60.de>
2025-12-08 19:50:02 +0100

    update

--- app/src/config.ts
+++ app/src/config.ts
@@ -1,7 +1,8 @@
 export const feedbackMail = atob('YW5kcm9pZEBqdTYwLmRl'); // email as base64
 export const buildDate = import.meta.env.VITE_BUILD_DATE;
 export const analyticsUrl = 'https://boomerang.ju60.de/share/z8KAHmGY/Kiel%20Live';
-export const darkMapStyle = '/map-style/dark.json';
-export const lightMapStyle = '/map-style/light.json';
+const tileServer = 'https://tiles.ju60.de/styles/v2';
+export const darkMapStyle = `${tileServer}/dark.json`;
+export const lightMapStyle = `${tileServer}/light.json`;
 export const natsServerUrl = import.meta.env.VITE_NATS_URL;
 export const DEBUG = (globalThis?.window as { DEBUG?: boolean })?.DEBUG || import.meta.env.DEV;
