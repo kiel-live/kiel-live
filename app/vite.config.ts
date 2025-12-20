56390e125ef4ab5c0f5e55408d9fe4ef5f41448a
Lukas Hass <lukas@slucky.de>
2025-12-20 13:57:53 +0100

    chore: add CSP

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -4,66 +4,85 @@ import vue from '@vitejs/plugin-vue';
 import IconsResolver from 'unplugin-icons/resolver';
 import Icons from 'unplugin-icons/vite';
 import Components from 'unplugin-vue-components/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
+import csp from 'vite-plugin-csp-guard';
 import { VitePWA } from 'vite-plugin-pwa';
 import WindiCSS from 'vite-plugin-windicss';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  envDir: '../',
-  build: {
-    sourcemap: true,
-  },
-  plugins: [
-    vue(),
-    VueI18nPlugin({
-      include: path.resolve(__dirname, 'src/locales/**'),
-    }),
-    WindiCSS(),
-    Icons({ compiler: 'vue3' }),
-    Components({
-      resolvers: [IconsResolver()],
-    }),
-    VitePWA({
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,png}'],
-      },
-      includeAssets: ['favicon.ico'],
-      manifest: {
-        name: 'Kiel Live',
-        short_name: 'Kiel Live',
-        description: 'Wo bleibt mein Bus?',
-        start_url: './',
-        display: 'standalone',
-        theme_color: '#2c3e50',
-        background_color: '#FFFFFF',
-        icons: [
-          {
-            src: './img/icons/android-chrome-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: 'img/icons/android-chrome-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-          {
-            src: 'img/icons/android-chrome-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable',
-          },
-        ],
-      },
-    }),
-  ],
-  resolve: {
-    alias: {
-      '~/': `${path.resolve(__dirname, 'src')}/`,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, '../');
+  return {
+    envDir: '../',
+    build: {
+      sourcemap: true,
     },
-  },
-  test: {
-    dir: path.resolve(__dirname, 'src'),
-  },
+    plugins: [
+      vue(),
+      VueI18nPlugin({
+        include: path.resolve(__dirname, 'src/locales/**'),
+      }),
+      WindiCSS(),
+      Icons({ compiler: 'vue3' }),
+      Components({
+        resolvers: [IconsResolver()],
+      }),
+      VitePWA({
+        workbox: {
+          globPatterns: ['**/*.{js,css,html,png}'],
+        },
+        includeAssets: ['favicon.ico'],
+        manifest: {
+          name: 'Kiel Live',
+          short_name: 'Kiel Live',
+          description: 'Wo bleibt mein Bus?',
+          start_url: './',
+          display: 'standalone',
+          theme_color: '#2c3e50',
+          background_color: '#FFFFFF',
+          icons: [
+            {
+              src: './img/icons/android-chrome-192x192.png',
+              sizes: '192x192',
+              type: 'image/png',
+            },
+            {
+              src: 'img/icons/android-chrome-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+            },
+            {
+              src: 'img/icons/android-chrome-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'any maskable',
+            },
+          ],
+        },
+      }),
+      csp({
+        dev: {
+          run: true, // Run the plugin in dev mode
+          outlierSupport: ['vue', 'tailwind'],
+        },
+        policy: {
+          'script-src-elem': ["'self'", 'https://boomerang.ju60.de'],
+          'style-src-elem': ["'self'", "'unsafe-inline'"],
+          'connect-src': ["'self'", env.VITE_NATS_URL, 'https://tiles.ju60.de'],
+          'worker-src': ["'self'", 'blob:'],
+        },
+        build: {
+          sri: true,
+        },
+      }),
+    ],
+    resolve: {
+      alias: {
+        '~/': `${path.resolve(__dirname, 'src')}/`,
+      },
+    },
+    test: {
+      dir: path.resolve(__dirname, 'src'),
+    },
+  };
 });
