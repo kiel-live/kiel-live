eff156d65e662d5b7e4b19aecc3379f95a50e853
Lukas Hass <lukas@slucky.de>
2026-02-07 23:01:42 +0100

    set default-src

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -67,6 +67,7 @@ export default defineConfig(({ mode }) => {
           outlierSupport: ['vue', 'tailwind'],
         },
         policy: {
+          'default-src': ["'none'"],
           'script-src-elem': ["'self'", 'https://boomerang.ju60.de'],
           'style-src-elem': [
             "'self'",
