55ecd4820823f9067f90500d4cba92531c335e51
Lukas Hass <lukas@slucky.de>
2026-02-07 18:09:28 +0100

    fix format

--- app/src/components/layout/SettingsContainer.vue
+++ app/src/components/layout/SettingsContainer.vue
@@ -1,7 +1,5 @@
 <template>
-  <div
-    class="flex flex-col min-h-0 grow overflow-y-auto p-2 md:max-w-200 md:mx-auto mt-(--safe-area-top,0px)"
-  >
+  <div class="flex flex-col min-h-0 grow overflow-y-auto p-2 md:max-w-200 md:mx-auto mt-(--safe-area-top,0px)">
     <slot />
   </div>
 </template>
