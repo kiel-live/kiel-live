70ab29a601c734f2b717207cc61c94a4db4678eb
Anbraten <anton@ju60.de>
2026-02-08 14:58:22 +0100

    adjust test

--- app/tests/safe-area.test.ts
+++ app/tests/safe-area.test.ts
@@ -1,7 +1,8 @@
+import type { Page } from '@playwright/test';
 import { expect, test } from '@playwright/test';
 import { setLiteMode, waitForMapToLoad } from './utils';
 
-async function injectSafeAreaVars(page: any) {
+async function injectSafeAreaVars(page: Page) {
   await page.addStyleTag({
     content: `
       :root {
@@ -24,7 +25,6 @@ test('Safe area on settings', async ({ page }) => {
   await page.goto('/');
   await injectSafeAreaVars(page);
   await page.getByRole('link', { name: 'Settings' }).click();
-  await expect(page.getByRole('link', { name: 'Follow @kiel.live' })).toBeVisible();
 
   await expect(page).toHaveScreenshot();
 });
@@ -33,14 +33,6 @@ test('Safe area in lite mode', async ({ page }) => {
   await setLiteMode(page);
   await injectSafeAreaVars(page);
   await page.getByRole('link', { name: 'Search' }).click();
-
-  // ensure map is not visible
-  await expect(page.getByRole('region', { name: 'Map' })).not.toBeVisible();
-
-  // ensure search is visible
-  await page.getByRole('textbox', { name: 'Search' }).click();
-  await page.getByRole('textbox', { name: 'Search' }).fill('haupt');
-  await expect(page.getByRole('heading', { name: 'Search result' })).toBeVisible();
 
   await expect(page).toHaveScreenshot();
 });
