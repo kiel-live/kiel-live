4b833278844637fddd373d7c5c5cf865fca86d95
Anbraten <anton@ju60.de>
2026-02-11 09:44:41 +0100

    limit tests to mobile only

--- app/tests/safe-area.test.ts
+++ app/tests/safe-area.test.ts
@@ -13,43 +13,45 @@ async function injectSafeAreaVars(page: Page) {
   });
 }
 
-test('Safe area on map', async ({ page }) => {
-  await page.goto('/');
-  await injectSafeAreaVars(page);
-  await waitForMapToLoad(page);
-
-  await expect(page).toHaveScreenshot();
-});
-
-test(
-  'Safe area on map with maximized popup',
+test.describe(
+  'Safe area',
   {
     tag: ['@mobile-only'],
   },
-  async ({ page }) => {
-    await page.goto('/');
-    await injectSafeAreaVars(page);
-    await waitForMapToLoad(page);
-    await page
-      .getByRole('button', { name: 'Drag to resize popup' })
-      .dragTo(page.getByRole('textbox', { name: 'Search' }));
+  () => {
+    test('Safe area on map', async ({ page }) => {
+      await page.goto('/');
+      await injectSafeAreaVars(page);
+      await waitForMapToLoad(page);
 
-    await expect(page).toHaveScreenshot();
+      await expect(page).toHaveScreenshot();
+    });
+
+    test('Safe area on map with maximized popup', async ({ page }) => {
+      await page.goto('/');
+      await injectSafeAreaVars(page);
+      await waitForMapToLoad(page);
+      await page
+        .getByRole('button', { name: 'Drag to resize popup' })
+        .dragTo(page.getByRole('textbox', { name: 'Search' }));
+
+      await expect(page).toHaveScreenshot();
+    });
+
+    test('Safe area on settings', async ({ page }) => {
+      await page.goto('/');
+      await injectSafeAreaVars(page);
+      await page.getByRole('link', { name: 'Settings' }).click();
+
+      await expect(page).toHaveScreenshot();
+    });
+
+    test('Safe area in lite mode', async ({ page }) => {
+      await setLiteMode(page);
+      await injectSafeAreaVars(page);
+      await page.getByRole('link', { name: 'Search' }).click();
+
+      await expect(page).toHaveScreenshot();
+    });
   },
 );
-
-test('Safe area on settings', async ({ page }) => {
-  await page.goto('/');
-  await injectSafeAreaVars(page);
-  await page.getByRole('link', { name: 'Settings' }).click();
-
-  await expect(page).toHaveScreenshot();
-});
-
-test('Safe area in lite mode', async ({ page }) => {
-  await setLiteMode(page);
-  await injectSafeAreaVars(page);
-  await page.getByRole('link', { name: 'Search' }).click();
-
-  await expect(page).toHaveScreenshot();
-});
