970b7305d5ffde6ea6226f3d836ecaaa5e63ea68
Anbraten <anton@ju60.de>
2026-03-30 11:11:20 +0200

    fix url

--- app/tests/stop.test.ts
+++ app/tests/stop.test.ts
@@ -3,7 +3,7 @@ import { setLiteMode, testColorScheme, waitForMapToLoad } from './utils';
 
 testColorScheme('Display stop details', async ({ page }) => {
   await page.clock.setFixedTime(new Date('2024-02-02T10:30:00'));
-  await page.goto('/map/stop/kvg-2387');
+  await page.goto('/map/bus-stop/kvg-2387');
   await expect(page.getByRole('heading', { name: 'Hauptbahnhof' })).toBeVisible();
   await waitForMapToLoad(page);
   await expect(page).toHaveScreenshot();
@@ -12,7 +12,7 @@ testColorScheme('Display stop details', async ({ page }) => {
 testColorScheme('Display stop details in lite mode', async ({ page }) => {
   await page.clock.setFixedTime(new Date('2024-02-02T10:30:00'));
   await setLiteMode(page);
-  await page.goto('/map/stop/kvg-2387');
+  await page.goto('/map/bus-stop/kvg-2387');
   await expect(page.getByRole('heading', { name: 'Hauptbahnhof' })).toBeVisible();
   await expect(page).toHaveScreenshot();
 });
